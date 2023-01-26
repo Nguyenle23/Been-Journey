731be8d873513f3c1baca72e844bc080a9049d05
Nguyenle23___ <beenbin2308@gmail.com>
2023-01-26 23:22:30 +0700

    Fix | Bug Socail Links

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaFacebook } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 
@@ -28,13 +28,22 @@ const SocialLinks = () => {
       id: 3,
       child: (
         <>
+          Facebook <FaFacebook size={30} />
+        </>
+      ),
+      href: "https://www.facebook.com/nguyenle23.iu/",
+    },
+    {
+      id: 4,
+      child: (
+        <>
           Mail <HiOutlineMail size={30} />
         </>
       ),
       href: "mailto:nguyenle23.iu@gmail.com",
     },
     {
-      id: 4,
+      id: 5,
       child: (
         <>
           Resume <BsFillPersonLinesFill size={30} />
@@ -49,7 +58,7 @@ const SocialLinks = () => {
     <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
       <ul>
         {linsk.map(({id, child, href, style, download}) => (
-          <li key={id} className={"flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" + " " + style}>
+          <li key={id} className={"flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" + style}>
             <a
               href={href}
               className="flex justify-between items-center w-full text-white"
