ea14083f56685f742cf102d4501a8697da573137
Nguyenle23___ <beenbin2308@gmail.com>
2023-01-31 01:31:28 +0700

    Update | Index api

--- client/src/apis/index.js
+++ client/src/apis/index.js
@@ -1,11 +1,17 @@
 import axios from 'axios';
 
-//get statistics table
-export const getStat = async() => {
-    const request = await axios.get(`http://localhost:5555/user/stats`, {
-        headers: {
-            token: "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
-        }
-    });
-    return request;
+export const loginWithGoogle = async () => {
+    const rootUrl = "https://accounts.google.com/o/oauth2/v2/auth";
+    const options = {
+        redirect_uri: "http://localhost:5000/auth/google",
+        client_id: "618519447606-v77bdn6ojtnjjqr6kv3dmsshd9cc37tn.apps.googleusercontent.com",
+        access_type: "offline",
+        response_type: "code",
+        prompt: "consent",
+        scope: [
+            "https://www.googleapis.com/auth/photoslibrary",
+            "https://www.googleapis.com/auth/photoslibrary.readonly",
+        ].join(" "),
+    };
+    return `${rootUrl}?${new URLSearchParams(options)}`;
 }
