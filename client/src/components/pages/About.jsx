6695f33d476f833ecfdf910717a88524f56b1987
Nguyenle23 <beenbin2308@gmail.com>
2023-01-31 21:20:26 +0700

    Update | About section

--- client/src/components/pages/About.jsx
+++ client/src/components/pages/About.jsx
@@ -2,29 +2,32 @@ import React from "react";
 
 const About = () => {
   return (
-    <div name="About" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+    <div
+      name="About"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Brief about Been
+          </p>
         </div>
 
         <p className="text-xl mt-20">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
-          tempore sequi consequatur obcaecati soluta molestias nisi
-          exercitationem perspiciatis explicabo cupiditate praesentium
-          aspernatur voluptatem delectus dolor harum aut fugiat dicta, odio
-          fugit dolorum! Aliquam est in maxime tempora illum mollitia nihil, id
-          perferendis animi, beatae hic delectus possimus rem voluptatibus
-          dignissimos.
+          My name is Nguyen Le a.k.a Been . I am a senior studying at
+          Internation University - VNU. I am interested in technology and
+          willing to figure out how it works. Until now, I have had a lot of
+          opportunities to work in developing Back-end web. Besides, I am
+          learning on my own path to expand more knowledge about implementing AI
+          to reality.
         </p>
 
         <br />
 
         <p className="text-xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id beatae
-          recusandae consectetur nobis voluptates voluptatum incidunt
-          praesentium. Unde, aliquid quidem impedit neque quos, aliquam incidunt
-          error ipsa recusandae, voluptate ratione.
+          I used to build and develop some projects with my friends. Those are
+          below this section. Thank you for check out my portfolio. If you have
+          any questions, please feel free to contact me. 
         </p>
       </div>
     </div>
