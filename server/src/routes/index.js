53003229c7b8b43642dc5ea6b3e0dfbd23e2c718
Nguyenle23___ <beenbin2308@gmail.com>
2023-01-31 15:03:47 +0700

    Update index.js

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,8 +1,10 @@
 
 const authRouter = require('./auth');
+const albumRouter = require('./album');
 
 function routes(app) {
   app.use('/auth', authRouter);
+  app.use('/album', albumRouter);
 }
 
 module.exports = routes;
