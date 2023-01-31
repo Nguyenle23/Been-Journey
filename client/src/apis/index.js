68ae738160ee67b2335cadfc5f5b6251a5e90439
Nguyenle23___ <beenbin2308@gmail.com>
2023-01-31 15:02:11 +0700

    Update index.js

--- client/src/apis/index.js
+++ client/src/apis/index.js
@@ -1,17 +1,21 @@
 import axios from 'axios';
 
-export const loginWithGoogle = async () => {
-    const rootUrl = "https://accounts.google.com/o/oauth2/v2/auth";
-    const options = {
-        redirect_uri: "http://localhost:5000/auth/google",
-        client_id: "618519447606-v77bdn6ojtnjjqr6kv3dmsshd9cc37tn.apps.googleusercontent.com",
-        access_type: "offline",
-        response_type: "code",
-        prompt: "consent",
-        scope: [
-            "https://www.googleapis.com/auth/photoslibrary",
-            "https://www.googleapis.com/auth/photoslibrary.readonly",
-        ].join(" "),
-    };
-    return `${rootUrl}?${new URLSearchParams(options)}`;
+// export const loginWithGoogle = async () => {
+//     const rootUrl = "https://accounts.google.com/o/oauth2/v2/auth";
+//     const options = {
+//         redirect_uri: "http://localhost:5000/auth/google",
+//         client_id: "618519447606-v77bdn6ojtnjjqr6kv3dmsshd9cc37tn.apps.googleusercontent.com",
+//         access_type: "offline",
+//         response_type: "code",
+//         prompt: "consent",
+//         scope: [
+//             "https://www.googleapis.com/auth/photoslibrary",
+//             "https://www.googleapis.com/auth/photoslibrary.readonly",
+//         ].join(" "),
+//     };
+//     return `${rootUrl}?${new URLSearchParams(options)}`;
+// }
+
+export const getAlbums = async (req, res) => {
+    return await axios.get('http://localhost:5000/album');
 }
