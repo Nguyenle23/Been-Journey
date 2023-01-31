928fa8c9d34dc45403e5bf999862b97117d72111
Nguyenle23 <beenbin2308@gmail.com>
2023-01-31 21:34:11 +0700

    Update | Contact section

--- client/src/components/pages/Contact.jsx
+++ client/src/components/pages/Contact.jsx
@@ -1,36 +1,46 @@
 import React from "react";
+// w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4
 
 const Contact = () => {
   return (
-    <div name="Contact" className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white">
+    <div
+      name="Contact"
+      className="bg-gradient-to-b from-gray-800 to-black w-full md:h-screen text-white"
+    >
       <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">Contact</p>
-          <p className="py-6">Submit to keep in touch with me</p>
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Contact
+          </p>
+          <p className="py-6">Feel free to keep in touch with me</p>
         </div>
 
         <div className="flex justify-center items-center">
-          <form action="https://getform.io/f/64743e39-1809-4c0d-aba5-f0349f9fee17" method="POST" className="flex flex-col w-full md:w-1/2">
+          <form
+            action="https://getform.io/f/64743e39-1809-4c0d-aba5-f0349f9fee17"
+            method="POST"
+            className="flex flex-col w-full md:w-1/2"
+          >
             <input
               type="text"
               name="name"
-              placeholder="Enter your name"
+              placeholder="Been want to know your name"
               className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             />
             <input
               type="text"
               name="email"
-              placeholder="Enter your email"
+              placeholder="Been want to contact vi your email"
               className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             />
             <textarea
               name="message"
               rows="10"
-              placeholder="Enter your message"
+              placeholder="Been want to hear your message"
               className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             ></textarea>
             <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
-              Let's Talk
+              Let's go
             </button>
           </form>
         </div>
