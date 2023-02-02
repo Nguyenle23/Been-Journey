4a760e67237e6968fd7e53a97c3a4305c8a4d79b
Nguyenle23 <beenbin2308@gmail.com>
2023-02-02 19:50:40 +0700

    Update WorkExperiences.jsx

--- client/src/components/pages/WorkExperiences.jsx
+++ client/src/components/pages/WorkExperiences.jsx
@@ -8,6 +8,7 @@ const WorkExperiences = () => {
   const workExperiences = [
     {
       id: 1,
+      title: "Calculator-For-T.M.C",
       src: app_cal,
       alt: "Calculator-For-T.M.C",
       demo: "",
@@ -15,6 +16,7 @@ const WorkExperiences = () => {
     },
     {
       id: 2,
+      title: "React-Onmovie",
       src: reactOnMovie,
       alt: "react-onmovie",
       demo: "",
@@ -22,6 +24,7 @@ const WorkExperiences = () => {
     },
     {
       id: 3,
+      title: "WAD-Project-Course",
       src: "https://raw.githubusercontent.com/Nguyenle23/WAD-Project-Course/main/Images/Client/home.png",
       alt: "WAD-Project-Course",
       demo: "",
@@ -29,6 +32,7 @@ const WorkExperiences = () => {
     },
     {
       id: 4,
+      title: "EcommerceWebsite-Nodejs",
       src: "https://raw.githubusercontent.com/Nguyenle23/Hotel-Management-System/main/src/public/img/ill_img.png",
       alt: "EcommerceWebsite_nodejs",
       demo: "https://glamourous-shop.herokuapp.com/",
@@ -36,6 +40,7 @@ const WorkExperiences = () => {
     },
     {
       id: 5,
+      title: "Hotel-Management-System",
       src: hotel,
       alt: "Hotel-Management-System",
       demo: "https://demo-hotel-live.herokuapp.com/",
@@ -43,9 +48,10 @@ const WorkExperiences = () => {
     },
     {
       id: 6,
+      title: "OOP-Project-Course",
       src: game,
       alt: "OOP-Project-Course",
-      demo: "",
+      demo: "https://drive.google.com/file/d/182sOYkuk1PEPbdTgXcFLstWkl30Jz1Yy/view?usp=share_link",
       code: "https://github.com/Nguyenle23/OOP-Project-Course",
     },
   ];
@@ -58,25 +64,52 @@ const WorkExperiences = () => {
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-            Some journeys
+            Been's Journey
           </p>
-          <p className="py-6">Those are some journeys that I used to develop, build, and design for course's project</p>
+          <p className="py-6">
+            Those are some journeys that I used to develop, build, and design
+            for course's project
+          </p>
         </div>
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {workExperiences.map(({ id, src, alt, demo, code }) => (
-            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+          {workExperiences.map(({ id, src, alt, demo, code, title }) => (
+            <div
+              key={id}
+              className="group shadow-md shadow-gray-600 rounded-lg hover:scale-105 duration-200"
+            >
               <img
                 src={src}
                 alt={alt}
-                className="rounded-md duration-200 hover:scale-105"
+                className="group-rounded-md"
+                title={title}
               />
+              <div className="border-2 border-gray-500">
+                <p className="text-sm font-bold text-center">{title}</p>
+              </div>
               <div className="flex items-center justify-center">
-                <button onClick={() => window.open(demo, "_blank")} className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  Demo
-                </button>
-                <button onClick={() => window.open(code, "_blank")} className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  More
-                </button>
+                {demo === "" ? (
+                  <button
+                    onClick={() => window.open(code, "_blank")}
+                    className="w-1/2 px-6 py-3 m-4"
+                  >
+                    Detail
+                  </button>
+                ) : (
+                  <>
+                    <button
+                      onClick={() => window.open(demo, "_blank")}
+                      className="w-1/2 px-6 py-3 m-4"
+                    >
+                      Demo
+                    </button>
+                    <button
+                      onClick={() => window.open(code, "_blank")}
+                      className="w-1/2 px-6 py-3 m-4"
+                    >
+                      Detail
+                    </button>
+                  </>
+                )}
               </div>
             </div>
           ))}
