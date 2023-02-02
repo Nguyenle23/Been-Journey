6e14ecd9ef6af3841fa6ba11ef9adaad58633176
Nguyenle23 <beenbin2308@gmail.com>
2023-02-02 19:50:37 +0700

    Update Contact.jsx

--- client/src/components/pages/Contact.jsx
+++ client/src/components/pages/Contact.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
       <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-            Contact
+            Been's Contact
           </p>
           <p className="py-6">Feel free to keep in touch with me</p>
         </div>
